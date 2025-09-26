5a23dbee328907b0177bdac0dfb0afb141d3e104
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2025-09-26 10:52:49 -0300

    Implementando rotas CRUD

--- projeto_fastapi/app.py
+++ projeto_fastapi/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from projeto_fastapi.schemas import (
     Message,
@@ -23,6 +23,10 @@ def read_root():
 
 @app.post('/users/', response_model=UserPublic, status_code=HTTPStatus.CREATED)
 def create_user(user: UserSchema):
+    # O '**' vai descompactar o resultado de user.model_dump()
+    # (q é um dicionário),
+    # passando seus pares chave-valor como argumentos
+    # para criar o novo objeto UserDB, adicionando o id
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
 
     database.append(user_with_id)
@@ -33,3 +37,30 @@ def create_user(user: UserSchema):
 @app.get('/users/', response_model=UserList, status_code=HTTPStatus.OK)
 def read_users():
     return {'user': database}
+
+
+@app.put(
+    '/users/{user_id}', response_model=UserPublic, status_code=HTTPStatus.OK
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found',
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', response_model=Message, status_code=HTTPStatus.OK
+)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found',
+        )
+    database.pop(user_id - 1)
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -38,3 +38,47 @@ def test_read_users(client):
     assert response.json() == {
         'user': [{'username': 'yuri', 'email': 'teste@teste.com'}],
     }
+
+
+def test_update_user(client):
+    # Primeiro, cria um usuário para garantir que o
+    # banco de dados não está vazio
+    client.post(
+        '/users/',
+        json={
+            'username': 'yuri',
+            'email': 'teste@teste.com',
+            'password': '1234',
+        },
+    )
+
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'yuri_updated',
+            'email': 'updated@teste.com',
+            'password': '4321',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'yuri_updated',
+        'email': 'updated@teste.com',
+    }
+
+
+def test_delete_user(client):
+    # Primeiro, cria um usuário para garantir que o
+    # banco de dados não está vazio
+    # client.post(
+    #     '/users/',
+    #     json={
+    #         'username': 'yuri',
+    #         'email': 'teste@teste.com',
+    #         'password': '1234',
+    #     },
+    # )
+
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
