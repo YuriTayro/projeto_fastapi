1675cb704c82bee3bdcaff20eb1293ca56100dbb
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2025-10-03 15:49:00 -0300

    inicio de async

--- projeto_fastapi/database.py
+++ projeto_fastapi/database.py
@@ -1,15 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from projeto_fastapi.settings import Settings
+
 
 from projeto_fastapi.settings import Settings
 
 # cria o motor de conexao
-engine = create_engine(Settings().DATABASE_URL)
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
 # fornece conexões individuais para cada
 # requisição, garantindo que elas sejam abertas
 # e fechadas corretamente.
-def get_session():  # pragma: no cover
-    with Session(engine) as session:
+# def get_session():  # pragma: no cover
+#     with Session(engine) as session:
+#         yield session
+
+async def get_session():
+    async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
+
